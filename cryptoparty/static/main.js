b8acccd4360995179c6d9b971b2cf9867a34d237
Samuel Carlisle <samuelcarlisle@gmail.com>
2013-07-04 07:11:47 +0200

    started porting to leaflet.js from google maps using latest verion 0.6 leaflet css and js, all of the main.js functions need rewriting according to http://leafletjs.com/reference.html

--- cryptoparty/static/main.js
+++ cryptoparty/static/main.js
@@ -2,89 +2,116 @@ var map;
 var geocoder;
 
 $(document).ready(function () {
+
+    var map;
+    var ajaxRequest;
+    var plotlist;
+    var plotlayers = [];
+
+    // set up the map
+    map = new L.Map('map');
+
+    // create the tile layer with correct attribution
+    var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib = 'Map data © OpenStreetMap contributors';
+    var osm = new L.TileLayer(osmUrl, {
+        minZoom: 0,
+        maxZoom: 18,
+        attribution: osmAttrib
+    });
+
+    map.setView(new L.LatLng(48.37, 10.89), 1);
+    map.addLayer(osm);
+    });
+
+    /*
     var mapOptions = {
-            zoom: 1, center: new google.maps.LatLng(48.37, 10.89),
-            mapTypeId: google.maps.MapTypeId.ROADMAP 
+        zoom: 1,
+        center: new google.maps.LatLng(48.37, 10.89),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    gmap = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     geocoder = new google.maps.Geocoder();
 
 
     // set default location if location is given in URL
-    if(default_location != 'None') {
-        geocoder.geocode({'address': default_location}, function(results, status) {
+    if (default_location != 'None') {
+        geocoder.geocode({
+            'address': default_location
+        }, function (results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
-                map.setCenter(results[0].geometry.location);
-                map.setZoom(10);
-            }
-            else {
+                gmap.setCenter(results[0].geometry.location);
+                gmap.setZoom(10);
+            } else {
                 console.log("geolocation error");
             }
         });
+
     }
 
     // get Cryptoparties for markers
     $.ajax({
         url: '/json/party',
-        success: function(result) {
+        success: function (result) {
             parties = jQuery.parseJSON(result)
-            $.each(parties, function(index, value) {
-                var ll = new google.maps.LatLng(value.position.coordinates[1], value.position.coordinates[0]);             
+            $.each(parties, function (index, value) {
+                var ll = new google.maps.LatLng(value.position.coordinates[1], value.position.coordinates[0]);
                 console.log(ll);
                 var m = new google.maps.Marker({
                     position: ll,
-                    map: map,
-                    title: value.name});
-                var i = new google.maps.InfoWindow({
-                    content: '<p><h4>'+value.name+'</h4></p>'+
-                             '<p><b>Street Address: </b>'+value.street_address+'</p>'+
-                             '<p><b>Date: </b>'+value.time+'</p>'+
-                             '<p><b>Additional Info: </b><a href="'+value.additional_info+'">[link]</a></p>'+
-                             '<p><b>Event Organizer: </b>'+value.organizer_email+'</p>'
+                    map: gmap,
+                    title: value.name
                 });
-                google.maps.event.addListener(m, 'click', function() {
+                var i = new google.maps.InfoWindow({
+                    content: '<p><h4>' + value.name + '</h4></p>' + '<p><b>Street Address: </b>' + value.street_address + '</p>' + '<p><b>Date: </b>' + value.time + '</p>' + '<p><b>Additional Info: </b><a href="' + value.additional_info + '">[link]</a></p>' + '<p><b>Event Organizer: </b>' + value.organizer_email + '</p>'
+                });
+                google.maps.event.addListener(m, 'click', function () {
                     i.open(map, m);
                 });
             });
         }
     });
+
 });
+*/
 
 
 // go to location
 
 function map_go(address) {
-    geocoder.geocode({'address': address}, function(results, status) {
+    geocoder.geocode({
+        'address': address
+    }, function (results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-            map.setCenter(results[0].geometry.location);
-            map.setZoom(10);
-        }
-        else {
+            gmap.setCenter(results[0].geometry.location);
+            gmap.setZoom(10);
+        } else {
             console.log("geolocation error");
         }
     });
 }
 
-$('#search_location_go').on('click', function() {
+$('#search_location_go').on('click', function () {
     map_go($('#search_location_text').val());
 });
 
-$('#search_location_form').on('submit', function() {
+$('#search_location_form').on('submit', function () {
     map_go($('#search_location_text').val());
     return false;
 });
 
 
-$('#subscription_search_btn').on('click', function() {
+$('#subscription_search_btn').on('click', function () {
     var address = $('#subscription_location_text').val();
-    geocoder.geocode({'address': address}, function(results, status) {
+    geocoder.geocode({
+        'address': address
+    }, function (results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-            map.setCenter(results[0].geometry.location);
-            map.setZoom(10);
+            gmap.setCenter(results[0].geometry.location);
+            gmap.setZoom(10);
             $('[name=subscription_lat]').val(String(results[0].geometry.location.lat()));
             $('[name=subscription_lon]').val(String(results[0].geometry.location.lng()));
-        }
-        else {
+        } else {
             console.log("geolocation error");
         }
     });
@@ -92,7 +119,7 @@ $('#subscription_search_btn').on('click', function() {
 });
 
 // submit subscription_form
-$('#subscription_form').on('submit', function() {
+$('#subscription_form').on('submit', function () {
     var formdata = {
         email: $('[name=subscription_email]').val(),
         lat: $('[name=subscription_lat]').val(),
@@ -100,21 +127,23 @@ $('#subscription_form').on('submit', function() {
     };
 
     $('#subscription_error').html("<div class=\"alert alert-info\">doing stuff...</div>");
-    
+
     $.ajax({
         type: 'POST',
         url: '/json/subscription/add',
-        data: {data: JSON.stringify(formdata)},
-        success: function(result, status) {
-            if(result == "OK") { 
+        data: {
+            data: JSON.stringify(formdata)
+        },
+        success: function (result, status) {
+            if (result == "OK") {
                 console.log(result);
-                $('#subscription_error').html("<div class=\"alert alert-success\">"+
-                    "<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>"+
-                    "<strong>Okay, then!</strong> You should receive a confirmation "+
+                $('#subscription_error').html("<div class=\"alert alert-success\">" +
+                    "<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>" +
+                    "<strong>Okay, then!</strong> You should receive a confirmation " +
                     "email shortly. Just click on the link in it and you're done!</div>")
             } else {
-                $('#subscription_error').html("<div class=\"alert alert-error\">"+
-                    "<strong>Oh no!</strong> Something was wrong. The server said: "+result+"</div>");
+                $('#subscription_error').html("<div class=\"alert alert-error\">" +
+                    "<strong>Oh no!</strong> Something was wrong. The server said: " + result + "</div>");
             }
         }
     });
@@ -122,6 +151,6 @@ $('#subscription_form').on('submit', function() {
     return false;
 });
 
-$('#addParty').on('show', function() {
+$('#addParty').on('show', function () {
     $('#add_party_iframe').attr('src', '/party/add');
 });
